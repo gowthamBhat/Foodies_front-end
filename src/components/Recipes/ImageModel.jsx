eb287678b4b5feeb528715fdb6ddb0ba75364689
Gowtham  H D <gowthambhat793@gmail.com>
2021-12-02 14:38:04 +0530

    image model

--- src/components/Recipes/ImageModel.jsx
+++ src/components/Recipes/ImageModel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { v4 as uuidv4 } from 'uuid'
 function ImageModel({ selectedImg, setSelectedImg }) {
   console.log(selectedImg)
 
@@ -9,7 +9,51 @@ function ImageModel({ selectedImg, setSelectedImg }) {
   }
   return (
     <div className="backdrop" onClick={handleClick}>
-      <img src={serverAddress + selectedImg.url} alt="enlarged recipe img" />
+      <div className="model-image-wrap">
+        <img src={serverAddress + selectedImg.url} alt="enlarged recipe img" />
+      </div>
+
+      <div className="model-recipe-info">
+        <div className="model-recipe-content">
+          <p className="model-recipeLabel">{selectedImg.label}</p>
+          <p>Recipe Source:{selectedImg.source}</p>
+          <p>
+            <i> ingredients </i>
+          </p>
+          {selectedImg.ingredients.map((x) => (
+            <p key={uuidv4()}>
+              {x.text}:{x.weight}
+            </p>
+          ))}
+
+          <p>
+            <i> dietLabels</i>
+          </p>
+          {selectedImg.dietLabels.map((x) => (
+            <span key={uuidv4()}>{x + ' '}</span>
+          ))}
+          <p>
+            <i> healthLabels</i>
+          </p>
+          {selectedImg.healthLabels.map((x) => (
+            <span key={uuidv4()}>{x + ' '}</span>
+          ))}
+          <p>
+            <i> meal type</i>
+          </p>
+          {selectedImg.mealType.map((x) => (
+            <span key={uuidv4()}>{x + ' '}</span>
+          ))}
+          <p>
+            <i> cuisine type</i>
+          </p>
+          {selectedImg.cuisineType.map((x) => (
+            <span key={uuidv4()}>{x + ' '}</span>
+          ))}
+          <p>making description</p>
+          <p>{selectedImg.makingDescription}</p>
+        </div>
+      </div>
     </div>
   )
 }
